36e25b4eee6e5f8c545887808175c64598eb8488
Lera <valeriiakolesnikova21@gmail.com>
2023-09-20 23:18:53 +0500

    homework 2 added

---
+++ homework/project/script.js
@@ -0,0 +1,50 @@
+new Vue({
+    el: "#app",
+    data: {
+        articles: [{
+                url: "img/article1.jpg",
+                alt: 'Kitchen',
+                desc: 'Kitchan Design',
+                subheading: 'Let’s Get Solution For Building Construction Work',
+                date: '26 December,2022',
+            },
+            {
+                url: "img/article2.jpg",
+                alt: 'Flat',
+                desc: 'Living Design',
+                subheading: `Low Cost Latest Invented Interior Designing Ideas.`,
+                date: '22 December,2022',
+            }, {
+                url: "img/article3.jpg",
+                alt: 'KiInteriortchen',
+                desc: 'Interior Design',
+                subheading: 'Best For Any Office &Business Interior Solution',
+                date: '25 December,2022',
+            }, {
+                url: "img/article1.jpg",
+                alt: 'Kitchen',
+                desc: 'Kitchan Design',
+                subheading: 'Let’s Get Solution For Building Construction Work',
+                date: '26 December,2022',
+            }, {
+                url: "img/article1.jpg",
+                alt: 'Kitchen',
+                desc: 'Kitchan Design',
+                subheading: 'Let’s Get Solution For Building Construction Work',
+                date: '26 December,2022',
+            }, {
+                url: "img/article1.jpg",
+                alt: 'Kitchen',
+                desc: 'Kitchan Design',
+                subheading: 'Let’s Get Solution For Building Construction Work',
+                date: '26 December,2022',
+            }
+        ]
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+});
