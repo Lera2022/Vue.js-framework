f411e8b4d6c85899fd622e7c52cddbe78a596e42
Lera <valeriiakolesnikova21@gmail.com>
2023-09-26 23:03:31 +0500

    lection 4 added

---
+++ Lesson 7. Lecture. Vue cli/vue-project/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome"/>
+    <!-- <HelloWorld msg="New item"/> -->
+<CalcComponent/>
+<p>Lorem ipsum dolor sit amet consectetur.</p>
+<CalcComponent/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import CalcComponent from './components/CalcComponent.vue';
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    CalcComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
