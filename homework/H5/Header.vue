846681ff8d2cbaaad9c8af26bc2fa8e7ec42d2e3
Lera <valeriiakolesnikova21@gmail.com>
2023-10-01 14:40:44 +0500

    homework 5 added

---
+++ homework/H5/Header.vue
@@ -0,0 +1,46 @@
+<template>
+    <header class="header center">
+        <a class="logo" href="index.html">
+            <img src="img/Logo.svg" alt="Interno Logo">
+            <h1 class="heading heading__logo">Interno</h1>
+        </a>
+        <nav>
+            <ul class="nav__header">
+                <li>
+                    <a href="index.html">Home</a>
+                </li>
+                <li>
+                    <a href="#">Project</a>
+                </li>
+                <li>
+                    <a href="blog.html">Blog</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.header {
+    display: flex;
+    justify-content: space-between;
+    padding-top: 61px;
+    padding-bottom: 57px;
+}
+
+.logo {
+    display: flex;
+    gap: 10px;
+}
+
+.nav__header {
+    display: flex;
+    justify-content: space-between;
+    width: 243px;
+    font-size: 20px;
+}
+</style>
---
+++ homework/H5/ProductDetails.vue
@@ -0,0 +1,40 @@
+<template>
+<p>название продукта - {{ name }}</p>
+<p>форматированную цену - {{ formattedPrice() }}</p>
+<p>статус доступности - 
+<template v-if="available">Available</template>
+<template v-else>Out of stock</template>
+</p>
+</template>
+
+<script>
+
+export default {
+  name: 'SuppliesList',
+  components: {
+    SupplyComponent,
+  },
+  data() {
+    return {
+      product: 
+        {
+          name: 'first',
+          price: 99.99,
+          available: true,
+        }
+    };
+  },
+  methods: {
+
+  },
+  computed: {
+    formattedPrice() {
+      return `$${this.price}`
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
