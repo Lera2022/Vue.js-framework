2c6b2559ee31aff1bbc044feaa52b8ae1af358d1
Lera <valeriiakolesnikova21@gmail.com>
2023-10-04 19:10:14 +0500

    6 homework added

---
+++ homework/vue-project/my-project/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Project msg="New item" />
+  </div>
+</template>
+
+<script>
+import Project from "./components/Project.vue";
+
+export default {
+  name: "App",
+  components: {
+    Project,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
